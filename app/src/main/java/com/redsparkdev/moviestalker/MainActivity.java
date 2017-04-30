e258901b61bb382eb16a0d109182dd57a9fac7c4
Red <redsparkdev@gmail.com>
2017-04-30 15:37:23 +0100

    Set up RecycleView

--- app/src/main/java/com/redsparkdev/moviestalker/MainActivity.java
+++ app/src/main/java/com/redsparkdev/moviestalker/MainActivity.java
@@ -2,12 +2,49 @@ package com.redsparkdev.moviestalker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyAdapter.MyAdapterOnClickHandler{
+    private RecyclerView recyclerView;
+    private TextView errorMessageDisplay;
+    private ProgressBar loadingIndicator;
+    private MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Finding an binding ids
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerview_movies);
+        errorMessageDisplay = (TextView)findViewById(R.id.error_message_display);
+        loadingIndicator = (ProgressBar)findViewById(R.id.loading_indicator);
+
+        //Will be using a GridLayout for displaying the images
+        int spanCount = 2;
+        GridLayoutManager layoutManager = new GridLayoutManager(this, spanCount, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        myAdapter = new MyAdapter(this);
+
+        recyclerView.setAdapter(myAdapter);
+
+        loadMovieData();
+    }
+
+    @Override
+    public void onClick(String movieId) {
+
+    }
+    private void  loadMovieData(){
+        String[] temp = {"1", "2,", "3"};
+        myAdapter.setMovieData(temp);
+
+
     }
 }
---
+++ app/src/main/java/com/redsparkdev/moviestalker/MyAdapter.java
@@ -0,0 +1,72 @@
+package com.redsparkdev.moviestalker;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Red on 30/04/2017.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyAdapterViewHolder> {
+
+    private String[] movieData;
+
+    private MyAdapterOnClickHandler clickHandler;
+
+    public interface MyAdapterOnClickHandler {
+        void onClick(String movieId);
+    }
+
+    public MyAdapter(MyAdapterOnClickHandler clickHandler){
+        this.clickHandler = clickHandler;
+    }
+
+    @Override
+    public MyAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutID = R.layout.movie_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutID, parent, shouldAttachToParentImmediately);
+        return new MyAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapterViewHolder holder, int position) {
+        String thisMoviesData = movieData[position];
+        holder.movieTextView.setText(thisMoviesData);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(movieData == null)
+            return 0;
+        return movieData.length;
+    }
+    public class MyAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public final TextView movieTextView;
+        public MyAdapterViewHolder(View view) {
+            super(view);
+            movieTextView = (TextView) view.findViewById(R.id.image_thumbnail);
+            view.setOnClickListener(this);
+        }
+    @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+            String movieInfo = movieData[adapterPosition];
+            clickHandler.onClick(movieInfo);
+
+        }
+    }
+    public void setMovieData(String [] movieData){
+        this.movieData = movieData;
+        notifyDataSetChanged();
+    }
+}
