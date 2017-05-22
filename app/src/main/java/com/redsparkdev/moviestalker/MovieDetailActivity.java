298cf43b381d85f94768dc95078cb2c6974a7a6f
Red <redsparkdev@gmail.com>
2017-05-22 18:05:09 +0100

    Fixed duplicate review/trailer after recreating activity

--- app/src/main/java/com/redsparkdev/moviestalker/MovieDetailActivity.java
+++ app/src/main/java/com/redsparkdev/moviestalker/MovieDetailActivity.java
@@ -3,6 +3,7 @@ package com.redsparkdev.moviestalker;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.PersistableBundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
@@ -23,7 +24,6 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-//TODO fix duplication of Trailers and Reviews
 public class MovieDetailActivity extends AppCompatActivity {
     private final static String TAG = MovieDetailActivity.class.getSimpleName().toString();
     private ImageView imageImageView;
@@ -34,22 +34,25 @@ public class MovieDetailActivity extends AppCompatActivity {
     private LinearLayout mainLayout;
     private MovieInfo movieInfo;
 
-    private static final String SAVE_NAME_KEY = "name";
-    private static final String SAVE_LINK_KEY = "link";
+    /**
+     * private static final String SAVE_NAME_KEY = "name";
+     * private static final String SAVE_LINK_KEY = "link";
+     * <p>
+     * private static ArrayList<String> trailer_Name;
+     * private static ArrayList<String> trailer_Link;
+     * <p>
+     * private static final String SAVE_AUTHOR_KEY = "author";
+     * private static final String SAVE_CONTENT_KEY = "content";
+     * <p>
+     * private static ArrayList<String> reviews_Author;
+     * private static ArrayList<String> review_Content;
+     **/
+    private static final String SAVE_TRAILERS_KEY = "trailers";
+    private static final String SAVE_REVIEWS_KEY = "reviews";
 
-    private static ArrayList<String> trailer_Name;
-    private static ArrayList<String> trailer_Link;
 
-    private static final String SAVE_AUTHOR_KEY = "author";
-    private static final String SAVE_CONTENT_KEY = "content";
-
-    private static ArrayList<String> reviews_Author;
-    private static ArrayList<String> review_Content;
-
-    private  ArrayList<TextView> trailerViews;
-    private  ArrayList<LinearLayout> reviewViews;
-
-
+    private ArrayList<TextView> trailerViews;
+    private ArrayList<LinearLayout> reviewViews;
 
 
     private static final int TRAILER_LOADER = 2;
@@ -80,8 +83,6 @@ public class MovieDetailActivity extends AppCompatActivity {
         if (parentActivity != null) {
             if (parentActivity.hasExtra("MovieInfoObject")) {
                 movieInfo = (MovieInfo) parentActivity.getSerializableExtra("MovieInfoObject");
-                loadTrailerData(movieInfo.getId());
-
 
 
                 //gets the data from the movie object
@@ -91,8 +92,47 @@ public class MovieDetailActivity extends AppCompatActivity {
                 ratingTextView.append(movieInfo.getVote_average());
                 overviewTextView.setText(movieInfo.getOverview());
 
+
+                if (savedInstanceState != null) {
+                    Log.v(TAG, ":savedInstanceState != null");
+                    if (savedInstanceState.getBoolean(SAVE_TRAILERS_KEY)) {
+                        showTrailers();
+                    } else {
+                        loadTrailerData(movieInfo.getId());
+                    }
+
+                    if (savedInstanceState.getBoolean(SAVE_REVIEWS_KEY)) {
+                        showReviews();
+                    } else {
+                        loadReviewData(movieInfo.getId());
+                    }
+                } else {
+                    Log.v(TAG, ":savedInstanceState == null");
+                    loadTrailerData(movieInfo.getId());
+                    loadReviewData(movieInfo.getId());
+                }
+
             }
         }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        if (!movieInfo.getTrailers().isEmpty()) {
+            showTrailers();
+        } else {
+            loadTrailerData(movieInfo.getId());
+        }
+
+        if (!movieInfo.getReviews().isEmpty()) {
+            showReviews();
+        } else {
+            loadReviewData(movieInfo.getId());
+        }
+
+
     }
 
     public MovieInfo getMovieInfoRefrance() {
@@ -100,12 +140,11 @@ public class MovieDetailActivity extends AppCompatActivity {
     }
 
 
-
-    public void showReviews(){
+    public void showReviews() {
         Log.v(TAG, ":showing reviews");
-        for(ReviewInfo review: movieInfo.getReviews()){
+        for (ReviewInfo review : movieInfo.getReviews()) {
             final LinearLayout layout = (LinearLayout) View.inflate(this, R.layout.review_list_item, null);
-            TextView autor = (TextView)layout.findViewById(R.id.tv_review_author);
+            TextView autor = (TextView) layout.findViewById(R.id.tv_review_author);
             TextView content = (TextView) layout.findViewById(R.id.tv_review_content);
             reviewViews.add(layout);
             autor.setText(review.getAuthor());
@@ -124,13 +163,13 @@ public class MovieDetailActivity extends AppCompatActivity {
         final String TYPE_VIDEO = "Trailer";
 
         //trailer_Name = new ArrayList<>();
-       // trailer_Link = new ArrayList<>();
+        // trailer_Link = new ArrayList<>();
 
         for (final TrailerInfo trailer : movieInfo.getTrailers()) {
             //For now only interested in trailers
             if (trailer.getType().equalsIgnoreCase(TYPE_VIDEO)) {
                 trailer.setLink(NetworkUtil.buildTrailerVideoUrl(trailer.getKey()));
-                final TextView text_view = (TextView) View.inflate(this, R.layout.trailer_list_item, null) ;
+                final TextView text_view = (TextView) View.inflate(this, R.layout.trailer_list_item, null);
                 trailerViews.add(text_view);
 
 
@@ -138,14 +177,14 @@ public class MovieDetailActivity extends AppCompatActivity {
                 text_view.setTag(trailer.getLink());
 
                 /** i find no need for this
-                if (!trailer.getName().isEmpty())
-                    trailer_Name.add(trailer.getName());
-                else
-                    trailer_Name.add("Name not found");
-                if (!trailer.getLink().toString().isEmpty())
-                    trailer_Link.add(trailer.getLink().toString());
-                else
-                    trailer_Link.add("no link");
+                 if (!trailer.getName().isEmpty())
+                 trailer_Name.add(trailer.getName());
+                 else
+                 trailer_Name.add("Name not found");
+                 if (!trailer.getLink().toString().isEmpty())
+                 trailer_Link.add(trailer.getLink().toString());
+                 else
+                 trailer_Link.add("no link");
 
                  **/
                 mainLayout.addView(text_view);
@@ -160,16 +199,68 @@ public class MovieDetailActivity extends AppCompatActivity {
                 });
             }
         }
-        //Making sure review load after the trailers :).
-        loadReviewData(movieInfo.getId());
 
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        //checking if trailer data was loaded
+        if (!movieInfo.getTrailers().isEmpty()) {
+            outState.putBoolean(SAVE_TRAILERS_KEY, true);
+        } else {
+            outState.putBoolean(SAVE_TRAILERS_KEY, false);
+        }
+        //checking if review data was loaded
+        if (!movieInfo.getReviews().isEmpty()) {
+            outState.putBoolean(SAVE_REVIEWS_KEY, true);
+        } else {
+            outState.putBoolean(SAVE_REVIEWS_KEY, false);
+        }
+
+        //removing view since they will be rebuilt
+        for (TextView view : trailerViews) {
+            mainLayout.removeView(view);
+        }
+        for (LinearLayout layout : reviewViews) {
+            mainLayout.removeView(layout);
+        }
+
+
+        /**
+         //Saving Trailer info
+         trailer_Name = new ArrayList<>();
+         trailer_Link = new ArrayList<>();
+         for(TextView view: trailerViews){
+         trailer_Name.add(view.getText().toString());
+         trailer_Link.add(view.getTag().toString());
+         }
+         outState.putStringArrayList(SAVE_NAME_KEY, trailer_Name);
+         outState.putStringArrayList(SAVE_LINK_KEY, trailer_Link);
+
+         //Saving comment info
+         reviews_Author = new ArrayList<>();
+         review_Content = new ArrayList<>();
+         for(LinearLayout layout: reviewViews){
+         TextView author = (TextView)layout.findViewById(R.id.tv_review_author);
+         reviews_Author.add(author.getText().toString());
+
+         TextView content = (TextView)layout.findViewById(R.id.tv_review_content);
+         review_Content.add(content.getText().toString());
+         }
+         outState.putStringArrayList(SAVE_AUTHOR_KEY, reviews_Author);
+         outState.putStringArrayList(SAVE_CONTENT_KEY, review_Content);
+         **/
+
+
+    }
 
     //TODO set up an error text view;
     public void showError() {
 
     }
+
     //TODO set up loading indicator
     public void showLoadingIndicator() {
 
@@ -187,15 +278,16 @@ public class MovieDetailActivity extends AppCompatActivity {
             loaderManager.restartLoader(TRAILER_LOADER, queryBundle, new FetchMovieTrailers(this));
         }
     }
-    private void loadReviewData(String id){
+
+    private void loadReviewData(String id) {
         Log.v(TAG, ":Loading review data");
         Bundle queryBundle = new Bundle();
         queryBundle.putString(NetworkUtil.ExtraData.ID_KEY, id);
         LoaderManager loaderManager = getSupportLoaderManager();
         Loader<ReviewInfo[]> reviewSearch = loaderManager.getLoader(REVIEW_LOADER);
-        if(reviewSearch == null){
+        if (reviewSearch == null) {
             loaderManager.initLoader(REVIEW_LOADER, queryBundle, new FetchMovieReviews(this));
-        }else{
+        } else {
             loaderManager.restartLoader(REVIEW_LOADER, queryBundle, new FetchMovieReviews(this));
         }
     }
